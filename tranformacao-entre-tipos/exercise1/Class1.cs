a0e2ebc2b97bace3f519eac9c53845912d853de4
carlatakagi <carlatakagi@gmail.com>
2022-08-19 15:05:34 -0400

    feat: adicionando exercicio

---
+++ tranformacao-entre-tipos/exercise1/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace exercise1;
+
+public class Class1
+{
+    public int intEntry = 42;
+    public double doubleResponse;
+    public void Transform()
+    {
+        doubleResponse = (double) intEntry;
+    }
+}
